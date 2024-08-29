a0b1e39eb16feaabb81afdc0463d9c85d685d74c
Aprilisn <srinurwulanaprilia@gmail.com>
2024-08-29 14:51:15 +0700

    update

--- js/script.js
+++ js/script.js
@@ -1,1 +1,32 @@
 // Ini Javascript
+
+function formValidation() {
+    // Ini cara ambil DOM
+    let name = document.getElementById('name').value;
+    // Ini kondisi pemisahnya
+    if (name == ''){
+        alert('Formnya kosong');
+        // Ini kondisi jika Formnya kosong
+        document.getElementById('result').innerHTML = '-';
+    } else{
+        // Ini kondisi jika Formnya diisi
+        document.getElementById('result').innerHTML = name;
+    }
+    console.log(name);
+}
+
+let indexSlide = 1;
+showBanner();
+
+function nextSlide(n) {
+
+}
+
+function showBanner(indexBanner) {
+    let listImage = document.getElementsByClassName('banner-img');
+    let index = 0
+    while (index < listImage.length){
+        index++;
+    }
+    console.log(listImage);
+}
